5d59dec3579a3728d83e58e64961cb5ae9f46724
Peter Downey <peterdowney@visistruct.com>
2026-02-23 19:28:16 -0500

    Updates

--- free-gift-bulk-coupon-generator.php
+++ free-gift-bulk-coupon-generator.php
@@ -6,7 +6,7 @@
  * Version: 1.5.0
  * Author: EngineScript
  * Requires at least: 6.5
- * Tested up to: 6.8
+ * Tested up to: 6.9
  * Requires PHP: 7.4
  * License: GPL-3.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
